7bc851fe223c0ebd1d853ab030feabb888c9056d
s_VIT_s <vsstepushin@icloud.com>
2021-03-14 21:00:12 +0400

    Практическое задание к 8 занятию

--- Interface_VKGB/Users/UserFoto/UserFotoCVController.swift
+++ Interface_VKGB/Users/UserFoto/UserFotoCVController.swift
@@ -9,26 +9,34 @@ import UIKit
 
 
 class UserFotoCVController: UICollectionViewController {
-    var UserF: UserProfile?
+    
+    var userF: UserProfile?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "\(String(describing: UserF!.userName)) \(String(describing: UserF!.userSurname))"
+        title = "\(String(describing: userF!.userName)) \(String(describing: userF!.userSurname))"
     }
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int  { 1 }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        UserF?.userPhotos.count ?? 0
+        userF?.userPhotos.count ?? 0
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "UserFoto", for: indexPath) as! UserFotoCVCell
-        let albumUser = UserF?.userPhotos[indexPath.row]
+        let albumUser = userF?.userPhotos[indexPath.row]
         cell.userFoto.image = albumUser
         return cell
     }
-
-  
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showAllUserPhoto",
+           let allFotoViewController = segue.destination as? AllFotoViewController,
+           let albumUserFoto = userF?.userPhotos {
+            allFotoViewController.albumUserFoto = albumUserFoto
+            let indexPath = collectionView.indexPathsForSelectedItems!.first!
+            allFotoViewController.selectedPhotoIndex = indexPath.row
+        }
+    }
 }
